b85d1862ec24cbf18edd507753d3292f93807d2c
FloPtitos <FloPtitos>
2018-12-01 16:00:53 +0100

    changement circuit

--- chargementCircuit.js
+++ chargementCircuit.js
@@ -7,4 +7,5 @@ imageObjet.onload = function(){
     canvas = document.getElementById("#myCanvas");
     context = canvas.getContext("2d");
     imageObjet.src = "Monaco.png";
+    context.drawImage(imageObjet,0,0);
 }

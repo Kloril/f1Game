f2b8f64179f7483be2da2f792fe972f0b5d73628
Kloril <maxime433.prevot@gmail.com>
2018-12-05 10:08:51 +0100

    menu fonctionnel

--- game.js
+++ game.js
@@ -5,15 +5,8 @@ window.onload = init;
 let canvas, ctx, w, h;
 let mousePos;
 let img1;
+let imgCircuit;
 
-
-
-
-/*$('#start').click(function() {
-  $('#optionsMenu').hide();
-  init();
-  $('#myCanvas').show();
-});*/
 
 
 var c= new Car();
@@ -21,18 +14,28 @@ var c= new Car();
 function init() {
   canvas = document.querySelector("#myCanvas"); // récupére la balise canvas d'id myCanvas
   ctx = canvas.getContext("2d");    // crée le contexte(ctx) 2d
-
+  $('#myCanvas').hide();
   w = canvas.width;     //récupère largeur et hauteur du canvas
   h = canvas.height;
+ 
 
+$('#start').click(function() {
+  $('#menu').hide();
+  init();
+  $('#myCanvas').show();
+});
+  
+imgCircuit= new Image();
 
+imgCircuit.src = "monaco.jpg";
+ctx.drawImage(imgCircuit,10,10,w,h);
 
   img1 = new Image();
 
   img1.onload = function() {
       requestAnimationFrame(mainloop);
   }
-  img1.src = "f1car.png"; //crée une image et lance l'animation
+  img1.src = "f1car.png"; //crée une image et lance l'animation*/
 }
 
 
@@ -41,7 +44,8 @@ let y = 10; // position initiale
 function mainloop() {  //fonction principale
   // on efface le canvas
   ctx.clearRect(0, 0, w, h);
-
+ 
+  
   dessineVoiture(c,img1); //on dessine la voiture
   listener('keydown',c);
   
